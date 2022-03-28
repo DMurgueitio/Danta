3392888180e4f4382ae93ebe684b84083157356f
FORMACION <FORMACION@RIOAPRREAFSD023.FormacionSena.RED>
2022-03-28 15:40:17 -0500

    sobre nosotros

---
+++ Sobre_Nosotros.php
@@ -0,0 +1,143 @@
+
+      
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FYPX81JMET"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FYPX81JMET');
+</script>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dantaventura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Squadfree - v4.6.0
+  * Template URL: https://bootstrapmade.com/squadfree-free-bootstrap-template-creative/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body id="sobre">
+  
+<header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html"><span>Dantaventura</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
+          <li><a class="nav-link scrollto" href="#about">Quiénes Somos</a></li>
+          <li><a class="nav-link scrollto" href="#portfolio">Galeria de Imágenes</a></li>
+         <li class="dropdown"><a href="#services"><span>Servicios</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Alojamientos</a></li>
+             <li><a href="#">Esparcimiento</a></li>
+              <li><a href="#">Comercio</a></li>
+              <li><a href="#">Otros</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contáctenos</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+<div class=" row sobreNosotros container-fluid">
+<div class="col-md-6">
+<img src="./assets/img/logodanta.png" alt="">
+</div>
+<div class="col-md-6">
+<h1>Quienes Somos</h1>
+</div>
+ 
+<table class="table table-hover">
+  <tr>
+    <th>Categoría</th>
+    <th>Modificar</th>
+  </tr>
+  <tr>
+    <td>Sobre la Danta</td>
+    <td><button type="button" class="btn"><i class="bi bi-pencil-square"></i></button></td>
+  </tr>
+  <tr>
+    <td>Historia</td>
+    <td><button type="button" class="btn"><i class="bi bi-pencil-square"></i></button></td>
+  </tr>
+  <tr>
+   <td>Donde queda ubicada</td>
+    <td><button type="button" class="btn"><i class="bi bi-pencil-square"></i></button></td>
+  </tr>
+  <tr>
+    <td>Como llegar</td>
+    <td><button type="button" class="btn"><i class="bi bi-pencil-square"></i></button></td>
+  </tr>
+  <tr>
+    <td>Atractivos turísticos</td>
+    <td><button type="button" class="btn"><i class="bi bi-pencil-square"></i></button></td>
+  </tr>
+</table>
+</div>
+
+    <!--<div class="col-md-6 d-flex justify-content-center bg-secondary mb-3 Primero ">
+      <form action="" method="POST">
+          <table class="table table-hover"> 
+              <thead>
+                  <tr>
+                      <th>Categoria</th>
+                      <th>Modificar</th>
+                  </tr>
+              </thead>
+                <tbody>
+                  <tr><td>Sobre la Danta          </td>
+                  <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-square"></i></button></td></tr>
+                  <tr><td>Historia                </td>
+                  <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-square"></i></button></td></tr>
+                  <tr><td>Donde queda Ubicada     </td>
+                  <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-square"></i></button></td></tr>
+                  <tr><td>Como Llegar             </td>
+                  <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-square"></i></button></td></tr>
+                  <tr><td>Atractivos Turisticos   </td>
+                  <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-square"></i></button></td></tr>
+                </tbody>
+          </table>
+      </form>
+    </div>-->
+</body>
+</html>
